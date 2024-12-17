d43e90b4a5aeb40f6a9bd4a4bbf42a845f006d40
Mutumba <rmutumba08@gmail.com>
2024-12-17 19:23:21 +0300

    added functions and variables

--- start.py
+++ start.py
@@ -2,3 +2,33 @@
 name = "Mutumba"
 
 print("Hello " +name)
+
+#dictionary
+
+person = {
+    "name": "MAK",
+    "age" : 24
+}
+print(person)
+print(person["name"])
+print(person["age"])
+
+# Variables
+number = 5
+num = 7
+
+def multiple():
+    return number*num
+
+print(multiple())
+
+#subtraction
+new=56
+old=46
+
+def sub():
+    return new-old
+
+print(sub())
+#functions are sorted
+
