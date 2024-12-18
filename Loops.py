17ff98a2517fb011e1520ba8f9b332a74a49826e
Mutumba <rmutumba08@gmail.com>
2024-12-19 01:23:13 +0300

    Python Loops

---
+++ Loops.py
@@ -0,0 +1,15 @@
+# Execute code repeatedly
+
+# for loop
+
+list_of_Fruits = ["Mango", "Apple", "Pineapple", "Lemon"]
+
+for item in list_of_Fruits:
+    print(item)
+
+#while loop
+x=5
+
+while(x>4):
+    print("High probablity")
+    break
