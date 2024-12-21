a390c302bb633acfb4234ae7ea07453005cb1a90
Mutumba <rmutumba08@gmail.com>
2024-12-21 12:38:06 +0300

    Methods innObject oriented programs

---
+++ Object.py
@@ -0,0 +1,30 @@
+#Object inbuilt methods
+#strings
+s="Hello"
+van=s.upper()
+print(van)
+
+#Incase you write a statement in uppercase and you want to turn it in lower case
+S1="CLASSIC"
+van1=S1.lower()
+print(van1)
+
+print(van,van1)
+
+#Count of characters
+Tale="I arreived in Nansana but"
+van2=Tale.count('n')
+print(van2)
+
+#Indices
+tail="John"
+vin=tail.index('o')
+print(vin)
+print(tail[3])
+print(len(tail))
+print(tail[1:3]) #It's called a string slice which brings out the String characters your interested in
+
+#Find method
+gift="Receive"
+hell=gift.find('v')
+print(hell)
