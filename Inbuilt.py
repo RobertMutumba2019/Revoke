22f65e6ba121f2148387e1e4a946da060849cbcc
mutumba <rmutumba08@gmail.com>
2025-01-21 21:23:21 +0300

    camelcase added

--- Inbuilt.py
+++ Inbuilt.py
@@ -1,6 +1,8 @@
 #In pytho, we have inbuilt modules or libraries used.
 import datetime
+import camelcase
 
+#This camelcase module converts texts into camelcase styles which is the way of writing texts without spaces.
 x=datetime.datetime.now()
 print(x)
 
@@ -10,3 +12,21 @@ y = max(5, 10, 25)
 
 print(x)
 print(y)
+
+
+
+c = camelcase.CamelCase()
+
+txt = "hello world"
+
+print(c.hump(txt))
+
+
+# Create a camelcase object
+c = camelcase.CamelCase()
+
+# Convert a string to CamelCase
+text = "helloworld from python"
+camel_case_text = c.hump(text)
+
+print(camel_case_text)  # Output: "helloWorldFromPython"
