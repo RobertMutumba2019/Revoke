2d27f6399bcabeea514fce0ffaacacd95a8363d8
mutumba <rmutumba08@gmail.com>
2025-01-21 21:11:28 +0300

    Modules in pythons

---
+++ Inbuilt.py
@@ -0,0 +1,5 @@
+#In pytho, we have inbuilt modules or libraries used.
+import datetime
+
+x=datetime.datetime.now()
+print(x)
---
+++ Module.py
@@ -0,0 +1,12 @@
+#In python modules also act as libraries
+
+def result(x,y):
+    return x*y
+
+res=result(5,6)
+print(res)
+
+def gone(van):
+    print("Hello "+van)
+
+gun=gone("Robert")
