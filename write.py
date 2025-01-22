685178bb9d5035623ff1c92231d02705d2b629e0
mutumba <rmutumba08@gmail.com>
2025-01-22 20:52:47 +0300

    Castings of datatypes

--- write.py
+++ write.py
@@ -2,3 +2,14 @@ f = open("demofile.txt", "a")
 f.write("Now the file has more content!")
 f.write("The text which must be typed")
 f.close()
+
+
+y = int(2.44)
+print(y)
+
+X=float(5)
+print(X)
+
+v=str(10)
+print(v)
+#In casting, we change the datatype of one format to another
